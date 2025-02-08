659f41084744f4a3cf9837516f9627137efe8318
omkar994 <76399492+omkar994@users.noreply.github.com>
2025-02-08 19:27:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-success">
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-1" to="/">KSpurs</Link>
+                    <Link className="navbar-brand fs-1" to="/">Spurs</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
